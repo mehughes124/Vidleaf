444298201602750fa02fa1fbc5353496687c6ab9
mehughes124 <mehughes124@gmail.com>
2023-08-07 19:09:38 -1000

    title edit

--- linkberree-laravel/resources/views/linkberree-lp.blade.php
+++ linkberree-laravel/resources/views/linkberree-lp.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Vertical Video Demo</title>
+        <title>Linkberree</title>
         <link href="{{ asset('/css/demo2.css') }}" rel="stylesheet">
     </head>
     <body class="snap-scrolling">
